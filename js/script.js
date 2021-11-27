7b2b78cc12882c0eded04d659a5ee300623ede6d
dzarrorn <zarrorn@gmail.com>
2021-11-28 02:57:54 +0700

    update

--- js/script.js
+++ js/script.js
@@ -5,19 +5,3 @@ document.querySelector('#burger').onclick = () => {
 window.onscroll = () => {
     navbar.classList.remove('active');
 }
-var swiper = new Swiper(".testimonial-slide", {
-    loop: true,
-    spaceBetween: 20,
-    autoplay: {
-        delay: 5000,
-        disableOnInteraction: false,
-    },
-    breakpoints: {
-        0: {
-            slidesPerView: 1,
-        },
-        768: {
-            slidesPerView: 1,
-        },
-    },
-});
